bc211bbad04051f136c5d014c73957398e5d3877
lostboy <paul.crabtree@gmail.com>
2010-12-02 13:48:29 +0530

    modified getaction url to get actions from same sub path

--- actions/getactions.rb
+++ actions/getactions.rb
@@ -5,11 +5,11 @@ newaction("getactions") do |cmd, ud, md, config|
         EC2Boot::Util.log("Fetching action list from #{url}")
 
         list = EC2Boot::Util.get_url(url)
-
+        action_url = url.split('/')[0..-2].join('/')
         list.split("\n").each do |command|
-            EC2Boot::Util.log("Fetching command: #{command}")
+            EC2Boot::Util.log("Fetching command: #{action_url}/#{command}")
 
-            body = EC2Boot::Util.get_url("#{url}/#{command}")
+            body = EC2Boot::Util.get_url("#{action_url}/#{command}")
 
             File.open(config.actions_dir + "/#{command}", "w") do |f|
                 f.print body
